3b3f1960db51468cb9e49072550f353cd117be18
Aabah94 <47247251+Aabah94@users.noreply.github.com>
2020-03-17 11:02:06 +0530

    Update seed.groovy

--- dsl/seed.groovy
+++ dsl/seed.groovy
@@ -2,6 +2,6 @@ buildMonitorView('project-A') {
     description('All jobs for project A')
     jobs {
         name('release-projectA')
-        regex(/project-A-.+/)
+        regex(/Test.+/)
     }
 }
